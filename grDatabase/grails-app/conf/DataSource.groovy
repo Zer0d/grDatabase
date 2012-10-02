8dedcd3268fff0fbc534c1ea3cc86cb9929a6fd7
Adrian Kelly <akelly@dius.com.au>
2012-10-02 22:02:31 +1000

    db

--- grDatabase/grails-app/conf/DataSource.groovy
+++ grDatabase/grails-app/conf/DataSource.groovy
@@ -1,70 +1,90 @@
 dataSource {
     pooled = true
+	logSql = false
+	show_sql = false
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
 }
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+    cache.use_query_cache = true
+    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
-environments {
-     development {
-        dataSource {
-            driverClassName = "org.h2.Driver"
-            username = "sa"
-            password = ""
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+    environments {
+        development {
+            dataSource {
+                pooled = true
+                dbCreate = "update"
+                url = "jdbc:mysql://localhost/zeroddbname?useUnicode=yes&characterEncoding=UTF-8"
+                driverClassName = "com.mysql.jdbc.Driver"
+                dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+                username = "zerodusername"
+                password = "zerodpassword"
+                properties {
+                    maxActive = 5
+                    maxIdle = 3
+                    minIdle = 1
+                    initialSize = 1
+                    removeAbandoned = true
+                    minEvictableIdleTimeMillis = 60000
+                    timeBetweenEvictionRunsMillis = 60000
+                    maxWait = 10000
+                    validationQuery = "SELECT 1"
+                    testOnBorrow=true
+                    testOnReturn=true
+                    testWhileIdle=true
+                }
+            }
         }
-    }
-    test {
-       dataSource {
-            pooled = true
-            dbCreate = "update"
-            url = "jdbc:mysql://localhost/zeroddbname?useUnicode=yes&characterEncoding=UTF-8"
-            driverClassName = "com.mysql.jdbc.Driver"
-            dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
-            username = "zerodusername"
-            password = "zerodpassword"
-            properties {
-                maxActive = 5
-                maxIdle = 3
-                minIdle = 1
-                initialSize = 1
-                removeAbandoned = true
-                minEvictableIdleTimeMillis = 60000
-                timeBetweenEvictionRunsMillis = 60000
-                maxWait = 10000
-                validationQuery = "SELECT 1"
-                testOnBorrow=true
-                testOnReturn=true
-                testWhileIdle=true
+        test {
+            dataSource {
+                pooled = true
+                dbCreate = "update"
+                url = "jdbc:mysql://localhost/zeroddbname?useUnicode=yes&characterEncoding=UTF-8"
+                driverClassName = "com.mysql.jdbc.Driver"
+                dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+                username = "zerodusername"
+                password = "zerodpassword"
+                properties {
+                    maxActive = 5
+                    maxIdle = 3
+                    minIdle = 1
+                    initialSize = 1
+                    removeAbandoned = true
+                    minEvictableIdleTimeMillis = 60000
+                    timeBetweenEvictionRunsMillis = 60000
+                    maxWait = 10000
+                    validationQuery = "SELECT 1"
+                    testOnBorrow=true
+                    testOnReturn=true
+                    testWhileIdle=true
+                }
             }
-       }
-    }
-    production {
-        dataSource {
-            pooled = true
-            dbCreate = "update"
-            url = "jdbc:mysql://localhost/zeroddbname?useUnicode=yes&characterEncoding=UTF-8"
-            driverClassName = "com.mysql.jdbc.Driver"
-            dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
-            username = "zerodusername"
-            password = "zerodpassword"
-            properties {
-                maxActive = 50
-                maxIdle = 25
-                minIdle = 5
-                initialSize = 5
-                removeAbandoned = true
-                minEvictableIdleTimeMillis = 60000
-                timeBetweenEvictionRunsMillis = 60000
-                maxWait = 10000
-                validationQuery = "SELECT 1"
-                testOnBorrow=true
-                testOnReturn=true
-                testWhileIdle=true
+        }
+        production {
+            dataSource {
+                pooled = true
+                dbCreate = "update"
+                url = "jdbc:mysql://localhost/zeroddbname?useUnicode=yes&characterEncoding=UTF-8"
+                driverClassName = "com.mysql.jdbc.Driver"
+                dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+                username = "zerodusername"
+                password = "zerodpassword"
+                properties {
+                    maxActive = 50
+                    maxIdle = 25
+                    minIdle = 5
+                    initialSize = 5
+                    removeAbandoned = true
+                    minEvictableIdleTimeMillis = 60000
+                    timeBetweenEvictionRunsMillis = 60000
+                    maxWait = 10000
+                    validationQuery = "SELECT 1"
+                    testOnBorrow=true
+                    testOnReturn=true
+                    testWhileIdle=true
+                }
             }
-         }
-    }
+        }
 }
